453cdba88857639aea0178b6dff4cdf235cb38d2
pottereric <eric.b.potter@gmail.com>
2014-06-23 13:40:48 -0400

    Added in chutzpah/jasmine

---
+++ ChessPuzzles/tests/KnightSolver_UnitTests/First.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="D:\Projects\ChessPuzzles\ChessPuzzles\Scripts/jasmine.js" />
+
+describe('KnightSolver', function () {
+
+    it('can fight dragon', function () {
+        //equal(15, 15, "should add 5");
+        expect(15).toBe(15);
+    });
+
+});
